893ed8e8b6c17e8cc653abefa55a4d3f9778841c
ThoCed <thonon_cedric@hotmail.com>
2018-01-22 23:03:33 +0100

    Override de la méthode add et clear dans le SelectedShip - permet d'activer le flag de selection

--- src/main/java/opera/modeles/SelectedShips.java
+++ src/main/java/opera/modeles/SelectedShips.java
@@ -1,14 +1,31 @@
 package opera.modeles;
 
-import opera.controllers.ShipBaseController;
-
 import java.util.ArrayList;
+import java.util.ListIterator;
+
+import opera.controllers.ShipBaseController;
 
 /**
  * @author Thoced
  * @see class dérivée de ArrayList, contient la liste des vaisseaux sélectionné
  * @param <ShipBaseController>
  */
-public class SelectedShips<ShipBaseController> extends ArrayList<ShipBaseController> {
+public class SelectedShips<Ship> extends ArrayList<ShipBaseController> {
 
+    @Override
+    public boolean add(ShipBaseController shipBaseController) {
+        shipBaseController.setSelected(true);
+
+     return super.add(shipBaseController);
+
+    }
+
+    @Override
+    public void clear() {
+        ListIterator<ShipBaseController> list = this.listIterator();
+        while(list.hasNext()){
+            list.next().setSelected(false);
+        }
+        super.clear();
+    }
 }
