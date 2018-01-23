ecf2cb0eac7262356388902e2643cd9d70f2515c
ThoCed <thonon_cedric@hotmail.com>
2018-01-23 23:18:42 +0100

    Class de la liste de chemins

---
+++ src/main/java/opera/modeles/PathTrip.java
@@ -0,0 +1,11 @@
+package opera.modeles;
+
+import java.util.*;
+
+/**
+ * Pile FIFO des chemins déterminés pour les déplacements
+ * @param <Vector3f>
+ */
+public class PathTrip<Vector3f> extends Stack<Vector3f> {
+
+}
